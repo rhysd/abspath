494cf2297745e5efd92fa72d98a75233df44976f
rhysd <lin90162@yahoo.co.jp>
2016-09-15 11:25:25 +0900

    install golint on Travis CI

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -6,13 +6,12 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew update
     brew upgrade go
     go get -t -d -v ./...
-    golint
-    go vet
     go test -v
 else
     go get github.com/axw/gocov/gocov
     go get github.com/mattn/goveralls
-    if ! go get code.google.com/p/go.tools/cmd/cover; then go get golang.org/x/tools/cmd/cover; fi
+    go get github.com/golang/lint/golint
+    if ! go get code.google.com/p/go.tools/cmd/cover > /dev/null ;then go get golang.org/x/tools/cmd/cover; fi
     go get -t -d -v ./...
     golint
     go vet
